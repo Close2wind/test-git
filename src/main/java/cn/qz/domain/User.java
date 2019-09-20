99eb954954dede1f0dc12407f9fdc4b82c264118
tom_glb <HelloWorld_glb@uestc.cn>
2019-09-20 21:19:56 +0800

    1

---
+++ src/main/java/cn/qz/domain/User.java
@@ -0,0 +1,26 @@
+package cn.qz.domain;
+
+/**
+ * @Author Q Z
+ * @date 2019/9/20-21:13
+ */
+public class User {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
